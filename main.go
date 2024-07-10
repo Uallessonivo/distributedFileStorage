7f6831151bd073c233f44a103f90529076cbadf2
Uallessonivo <uallessons@gmail.com>
2024-07-10 15:13:24 -0300

    feat: Add TCPTransport initialization and listening functionality

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"githugb/Uallessonivo/distributedFileStorage/p2p"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
 }
